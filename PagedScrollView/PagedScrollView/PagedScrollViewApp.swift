1962dd92a0b1605ad56f23688111724fbd65252e
pilotns <pilot.ua@gmail.com>
2023-05-08 14:04:03 +0300

    Create project

---
+++ PagedScrollView/PagedScrollView/PagedScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagedScrollViewApp.swift
+//  PagedScrollView
+//
+//  Created by pilotns on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PagedScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
